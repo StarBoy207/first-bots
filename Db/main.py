75f5a61af773c030fbd790b6e061364134b1d98f
StarBoy207 <100226477+StarBoy207@users.noreply.github.com>
2022-02-23 06:38:47 +0500

    Add files via upload

---
+++ Db/main.py
@@ -0,0 +1,11 @@
+from telebot import *
+
+bot = telebot.TeleBot("Token")
+@bot.message_handler(commands=["start"])
+def reply_start(msg):
+    user = msg.from_user.first_name
+    link = msg.from_user.username
+    
+    bot.send_message(cid, )
+
+bot.infinity_polling()
